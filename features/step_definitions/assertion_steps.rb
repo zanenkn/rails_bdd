425a0390f46de7a0fda190c87062bafd93fe31e7
zanenkn <zane.neikena@gmail.com>
2019-04-27 17:38:26 +0200

    Adds an assertion step for being on an article page

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -1,3 +1,7 @@
 Then("I should see {string}") do |content|
     expect(page).to have_content content
-  end
+end
+
+Then("I should be on {string} page") do |content|
+    expect(page).to have_content content
+endg

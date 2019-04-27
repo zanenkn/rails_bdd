54bb05413d6d84f7d1f61af60591666b54a5428b
zanenkn <zane.neikena@gmail.com>
2019-04-27 22:46:24 +0200

    Generates Devise model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'articles/index'
   root controller: :articles, action: :index
   resources :articles, only: [:index, :new, :create, :show]

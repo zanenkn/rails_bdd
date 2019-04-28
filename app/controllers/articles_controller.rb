e85de981df2a7a9fc1a8e776df222d29aa69f4f6
zanenkn <zane.neikena@gmail.com>
2019-04-28 21:11:42 +0200

    Adds a delete button to the article form and a container for notice to be displayed in index.html.erb, as well as defines the displaying of the notice for 'destroy' action in the ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to_articles_path
+    redirect_to articles_path, notice: "Article was successfully deleted."
   end
 
 

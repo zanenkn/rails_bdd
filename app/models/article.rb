9e745939f74d6852d5a35f441ccc57493e11680b
zanenkn <zane.neikena@gmail.com>
2019-05-04 20:17:58 +0200

    Sets up an association between Article and Comment models, configures a nested route.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
+    has_many :comments
     validates_presence_of :title, :content
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'articles/index'
   root controller: :articles, action: :index
   resources :articles, only: [:index, :new, :create, :show]
+  
+  resources :articles do
+    resources :comments
+  end
+  
 end

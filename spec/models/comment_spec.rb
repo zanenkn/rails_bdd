9f55a16c5b88c791c528008259060e9cc393821b
zanenkn <zane.neikena@gmail.com>
2019-05-05 01:13:01 +0200

    Adds a unit test for validation of email.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe Comment, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of :commenter }
     it { is_expected.to validate_presence_of :body }
+    it { is_expected.to validate_format_of :email}
   end
 end

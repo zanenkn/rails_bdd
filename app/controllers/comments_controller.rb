f638b464ec4c1953be5d71b3b819bbca2ab18eba
zanenkn <zane.neikena@gmail.com>
2019-05-05 00:51:08 +0200

    Adds a conditional statement into the create method for Comments controller that displays either success or failure message after user tries to create a comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to article_path(@article), notice: "Comment posted successfully."
+    else
+      redirect_to article_path(@article), notice: "Failed to post comment. Make sure you fill in 'Name' and 'Comment' fields."
+    end
+    
   end
  
   private

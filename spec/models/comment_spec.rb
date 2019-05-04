52689643bccbfb0e6912b8f6fddb474d54d1814b
zanenkn <zane.neikena@gmail.com>
2019-05-04 23:29:30 +0200

    Creates a unit test for Comment model, adds a test for DB column existence (commenter, email, body) (passes) and validation of commenter and body (fails).

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :commenter }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :body }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :commenter }
+    it { is_expected.to validate_presence_of :body }
+  end
 end
